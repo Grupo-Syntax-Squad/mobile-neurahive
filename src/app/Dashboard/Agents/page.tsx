8ebfa007e5854be1ed84fc14713b9a12b9736909
WellingtonLFaria <wellingtonll.faria@gmail.com>
2025-05-26 22:14:57 -0300

    fix: agent dashboard page

--- src/app/Dashboard/Agents/page.tsx
+++ src/app/Dashboard/Agents/page.tsx
@@ -1,128 +1,166 @@
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from "react-native"
-import { Link, useRouter } from "expo-router"
 import React, { useEffect, useState } from "react"
 import { useAxios } from "@/contexts/axiosContext"
 import globalStyles from "../../styles/globalStyles"
-import { Agent } from "@/types/Agent"
+import { Agent, AgentKeys, PutAgentKeys, PutAgentRequest } from "@/types/Agent"
 import { getErrorMessage } from "@/utils/getErrorMessage"
-import Entypo from '@expo/vector-icons/Entypo';
-import { Text as SvgText} from 'react-native-svg'
-import { PieChart } from 'react-native-svg-charts'
+import Entypo from "@expo/vector-icons/Entypo"
+import { Text as SvgText } from "react-native-svg"
+import { PieChart } from "react-native-svg-charts"
 import AgentsLegend from "@/components/AgentsLegend"
 
-
 export function ManageAgents() {
-    const router = useRouter()
     const [agents, setAgents] = useState<Agent[]>([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState<string | null>(null)
-    const { get, deletar } = useAxios()
+    const [, setLoading] = useState(true)
+    const [, setError] = useState<string | null>(null)
+    const { get, deletar, put } = useAxios()
 
-    const enableAgents = agents.filter((agent) => agent.enabled);
-    const disableAgents = agents.filter((agent) => !agent.enabled);
+    const enableAgents = agents.filter((agent) => agent[AgentKeys.ENABLED])
+    const disableAgents = agents.filter((agent) => !agent[AgentKeys.ENABLED])
 
-    const percentActive = agents.length ? (enableAgents.length / agents.length) * 100 : 0;
-    const percentInactive = agents.length ? (disableAgents.length / agents.length) * 100 : 0;
+    const percentActive = agents.length ? (enableAgents.length / agents.length) * 100 : 0
+    const percentInactive = agents.length ? (disableAgents.length / agents.length) * 100 : 0
 
     const fetchAgents = async () => {
-            try {
-                setLoading(true)
-                const response = await get(`/agents/`)
-                setAgents(response.data)
-            } catch (err) {
-                setError("Erro ao carregar usuários")
-                console.error("Erro na requisição:", getErrorMessage(err))
-            } finally {
-                setLoading(false)
-            }
+        try {
+            setLoading(true)
+            const response = await get(`/agents/`, { params: { disabled_agents: true } })
+            setAgents(response.data)
+        } catch (err) {
+            setError("Erro ao carregar usuários")
+            console.error("Erro na requisição:", getErrorMessage(err))
+        } finally {
+            setLoading(false)
         }
+    }
 
-        const handleDelete = async (agentId: number) => {
-            try {
-                setLoading(true);
-                await deletar(`/agents/?agent_id=${agentId}`);
-                await fetchAgents();
-            } catch (err) {
-                console.error("Erro ao deletar:", getErrorMessage(err));
-                setError("Erro ao deletar Agente");
-            } finally {
-                setLoading(false);
-            }
-        };
-    
-        useEffect(() => {
-            fetchAgents()
-        }, [])
+    const handleDelete = async (agentId: number) => {
+        try {
+            setLoading(true)
+            await deletar(`/agents/?agent_id=${agentId}`)
+            await fetchAgents()
+        } catch (err) {
+            console.error("Erro ao deletar:", err)
+            setError("Erro ao deletar Agente")
+        } finally {
+            setLoading(false)
+        }
+    }
 
-        const data = [percentActive, percentInactive]
-        const chartColors = ['#16ae03', '#ae1a03']
-        const pieData = data.map((value, index) => ({
+    const handleEnable = async (agent: Agent) => {
+        const data: PutAgentRequest = {
+            [PutAgentKeys.NAME]: agent[AgentKeys.NAME],
+            [PutAgentKeys.THEME]: agent[AgentKeys.THEME],
+            [PutAgentKeys.BEHAVIOR]: agent[AgentKeys.BEHAVIOR],
+            [PutAgentKeys.TEMPERATURE]: agent[AgentKeys.TEMPERATURE],
+            [PutAgentKeys.TOP_P]: agent[AgentKeys.TOP_P],
+            [PutAgentKeys.IMAGE_ID]: agent[AgentKeys.IMAGE_ID],
+            [PutAgentKeys.GROUPS]: agent[AgentKeys.GROUPS],
+            [PutAgentKeys.KNOWLEDGE_BASE_ID]: agent[AgentKeys.KNOWLEDGE_BASE_ID],
+            [PutAgentKeys.ENABLED]: true,
+        }
+        try {
+            setLoading(true)
+            await put(`/agents/${agent[AgentKeys.ID]}`, data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            })
+            await fetchAgents()
+        } catch (err) {
+            console.error("Erro ao habilitar o agente:", err)
+            setError("Erro ao habilitar o agente")
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchAgents()
+    }, [])
+
+    const data = [percentActive, percentInactive]
+    const chartColors = ["#16ae03", "#ae1a03"]
+    const pieData = data
+        .map((value, index) => ({
             value: parseFloat(value.toFixed(0)),
             color: chartColors[index % chartColors.length],
         }))
-        .filter(item => item.value > 0)
+        .filter((item) => item.value > 0)
         .map((item, index) => ({
             value: item.value,
             key: `${index}-${item.value}`,
             svg: {
-            fill: item.color,
+                fill: item.color,
             },
-        }));
+        }))
 
-        const Label = ({ slices }) => {
-        return slices.map((slice, index) => {
-            const { pieCentroid, data } = slice;
+    const Label = ({ slices }: { slices: any }) => {
+        return slices.map((slice: any, index: any) => {
+            const { pieCentroid, data } = slice
             return (
-            <SvgText
-                key={`label-${index}`}
-                x={pieCentroid[0]}
-                y={pieCentroid[1]}
-                fill="white"
-                textAnchor={'middle'}
-                alignmentBaseline={'middle'}
-                fontSize={12}
-            >
-                {data.value}%
-            </SvgText>
-            );
-        });
-        };
-    
+                <SvgText
+                    key={`label-${index}`}
+                    x={pieCentroid[0]}
+                    y={pieCentroid[1]}
+                    fill="white"
+                    textAnchor={"middle"}
+                    alignmentBaseline={"middle"}
+                    fontSize={12}
+                >
+                    {data.value}%
+                </SvgText>
+            )
+        })
+    }
 
-  return (
-    <ScrollView keyboardShouldPersistTaps="handled"
-        contentContainerStyle={styles.scrollContainer}
-    >
-        <View style={{ justifyContent: 'center' }}>
-            <PieChart style={{ height: 150 }} data={pieData}>
-                <Label/>
-            </PieChart>
-        </View>
-        <AgentsLegend/>
-    <View style={styles.usersList}>
-        <Text style={globalStyles.orangeText}>Agentes Ativos</Text>
-        {agents.map((agent) => (
-            <AgentCard key={agent.id} agent={agent} onDelete={handleDelete}/>
-        ))}
-    </View>
-    </ScrollView>
-  );
-};
-
-type AgentCardProps = {
-    agent: Agent;
-    onDelete: (id: string) => void;
+    return (
+        <ScrollView
+            keyboardShouldPersistTaps="handled"
+            contentContainerStyle={styles.scrollContainer}
+        >
+            <View style={{ justifyContent: "center" }}>
+                <PieChart style={{ height: 150 }} data={pieData}>
+                    <Label />
+                </PieChart>
+            </View>
+            <AgentsLegend />
+            <View style={styles.usersList}>
+                <Text style={globalStyles.orangeText}>Agentes Ativos</Text>
+                {agents.map((agent) => (
+                    <AgentCard
+                        key={agent[AgentKeys.ID]}
+                        agent={agent}
+                        onDelete={handleDelete}
+                        onEnable={handleEnable}
+                    />
+                ))}
+            </View>
+        </ScrollView>
+    )
 }
 
-const AgentCard: React.FC<AgentCardProps> = ({ agent, onDelete }) => (
-    <View style={styles.userContainer}>
-        <Text style={styles.userName}>{agent.name}</Text>
-        <TouchableOpacity style={styles.userDetailButton}
-            onPress={() => onDelete(agent.id)}
-        >
-            <Entypo name="trash" size={24} color="white" />
-            
-        </TouchableOpacity>
+type AgentCardProps = {
+    agent: Agent
+    onDelete: (id: number) => Promise<void>
+    onEnable: (agent: Agent) => Promise<void>
+}
+
+const AgentCard: React.FC<AgentCardProps> = ({ agent, onDelete, onEnable }) => (
+    <View style={agent[AgentKeys.ENABLED] ? styles.userContainer : styles.disabledUserContainer}>
+        <Text style={styles.userName}>{agent[AgentKeys.NAME]}</Text>
+        {agent[AgentKeys.ENABLED] ? (
+            <TouchableOpacity
+                style={styles.disableAgentButton}
+                onPress={() => onDelete(agent[AgentKeys.ID])}
+            >
+                <Entypo name="trash" size={24} color="white" />
+            </TouchableOpacity>
+        ) : (
+            <TouchableOpacity style={styles.enableAgentButton} onPress={() => onEnable(agent)}>
+                <Entypo name="check" size={24} color="white" />
+            </TouchableOpacity>
+        )}
     </View>
 )
 
@@ -160,7 +198,6 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "#e0e0e0",
         alignItems: "center",
-
     },
     userName: {
         fontSize: 16,
@@ -176,7 +213,7 @@ const styles = StyleSheet.create({
         marginBottom: 12,
         color: "#333",
     },
-    userDetailButton: {
+    disableAgentButton: {
         backgroundColor: "#ff0000",
         padding: 10,
         borderRadius: 6,
@@ -184,6 +221,25 @@ const styles = StyleSheet.create({
         minWidth: 50,
         alignItems: "center",
     },
+    enableAgentButton: {
+        backgroundColor: "#3333ff",
+        padding: 10,
+        borderRadius: 6,
+        alignSelf: "flex-start",
+        minWidth: 50,
+        alignItems: "center",
+    },
+    disabledUserContainer: {
+        backgroundColor: "#fff",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderRadius: 8,
+        padding: 16,
+        borderWidth: 1,
+        borderColor: "#e0e0e0",
+        alignItems: "center",
+        opacity: 0.7,
+    },
 })
 
-export default ManageAgents;
+export default ManageAgents
--- src/types/Agent.ts
+++ src/types/Agent.ts
@@ -1,5 +1,49 @@
+export enum AgentKeys {
+    ID = "id",
+    NAME = "name",
+    THEME = "theme",
+    BEHAVIOR = "behavior",
+    TEMPERATURE = "temperature",
+    TOP_P = "top_p",
+    IMAGE_ID = "image_id",
+    GROUPS = "groups",
+    KNOWLEDGE_BASE_ID = "knowledge_base_id",
+    ENABLED = "enabled",
+}
+
 export interface Agent {
-    id: string
-    name: string
-    enabled: boolean
+    [AgentKeys.ID]: number
+    [AgentKeys.NAME]: string
+    [AgentKeys.THEME]: string
+    [AgentKeys.BEHAVIOR]: string
+    [AgentKeys.TEMPERATURE]: number
+    [AgentKeys.TOP_P]: number
+    [AgentKeys.IMAGE_ID]?: number
+    [AgentKeys.GROUPS]: number[]
+    [AgentKeys.KNOWLEDGE_BASE_ID]: number
+    [AgentKeys.ENABLED]: boolean
+}
+
+export enum PutAgentKeys {
+    NAME = "name",
+    THEME = "theme",
+    BEHAVIOR = "behavior",
+    TEMPERATURE = "temperature",
+    TOP_P = "top_p",
+    IMAGE_ID = "image_id",
+    GROUPS = "groups",
+    KNOWLEDGE_BASE_ID = "knowledge_base_id",
+    ENABLED = "enabled",
+}
+
+export interface PutAgentRequest {
+    [AgentKeys.NAME]: string
+    [AgentKeys.THEME]: string
+    [AgentKeys.BEHAVIOR]: string
+    [AgentKeys.TEMPERATURE]: number
+    [AgentKeys.TOP_P]: number
+    [AgentKeys.IMAGE_ID]?: number
+    [AgentKeys.GROUPS]: number[]
+    [AgentKeys.KNOWLEDGE_BASE_ID]: number
+    [AgentKeys.ENABLED]: boolean
 }
